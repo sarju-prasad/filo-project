32167d85d08aac48f3031c8ae20a8fbeb9fabdd1
sarju-prasad <sarjuprasadid2628@gmail.com>
2024-02-10 20:11:11 +0530

    addition file delete

--- src/compnents/barchart.js
+++ src/compnents/barchart.js
@@ -5,6 +5,9 @@ export default function BarChart(){
     return(
         <>
             <h1>Welcome to Bar Chart</h1>
+
+            <p>Lorem40gfkjhgdfgkdafklh sdhkjgdhkldmfgd
+            </p>
         </>
     );
 }
